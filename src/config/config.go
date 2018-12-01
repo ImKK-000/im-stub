fb0bf8eccf17150ae6bf9cb893b1cd8107f40ec1
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2018-12-01 18:05:38 +0700

    [Added]: default model Configs when error case

--- src/config/config.go
+++ src/config/config.go
@@ -8,6 +8,12 @@ import (
 	"gopkg.in/go-playground/validator.v9"
 )
 
+var defaultConfigs = model.Configs{
+	Host:      "0.0.0.0",
+	Port:      9999,
+	EnableLog: false,
+}
+
 func validateData(data interface{}) error {
 	validate := validator.New()
 	return validate.Struct(data)
@@ -16,16 +22,16 @@ func validateData(data interface{}) error {
 func ReadConfig(configPath string) (model.Configs, error) {
 	rawConfigs, errorReadConfigFile := ioutil.ReadFile(configPath)
 	if errorReadConfigFile != nil {
-		return model.Configs{}, errorReadConfigFile
+		return defaultConfigs, errorReadConfigFile
 	}
 	var configs model.Configs
 	errorConvertJSONToStruct := json.Unmarshal(rawConfigs, &configs)
 	if errorConvertJSONToStruct != nil {
-		return model.Configs{}, errorConvertJSONToStruct
+		return defaultConfigs, errorConvertJSONToStruct
 	}
 	errorValidateData := validateData(configs)
 	if errorValidateData != nil {
-		return model.Configs{}, errorValidateData
+		return defaultConfigs, errorValidateData
 	}
 	return configs, nil
 }
